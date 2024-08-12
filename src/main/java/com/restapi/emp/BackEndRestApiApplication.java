41bca3c2667bbbd5a6063520b5b52c86e6e2d2a8
eunsol-k <solsol1203@gmail.com>
2024-08-12 14:46:54 +0900

    JPA N+1 Problem: dependency add

--- src/main/java/com/restapi/emp/BackEndRestApiApplication.java
+++ src/main/java/com/restapi/emp/BackEndRestApiApplication.java
@@ -1,7 +1,9 @@
 package com.restapi.emp;
 
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackEndRestApiApplication {
@@ -9,5 +11,11 @@ public class BackEndRestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndRestApiApplication.class, args);
 	}
-
+	// Jackson Datatype Hibernate의 Hibernate6Module 객체를 Spring Bean으로 등록
+	@Bean
+	Hibernate6Module hibernate6Module() {
+		Hibernate6Module hibernate6Module = new Hibernate6Module();
+		hibernate6Module.configure(Hibernate6Module.Feature.FORCE_LAZY_LOADING,true);
+		return hibernate6Module;
+	}
 }
